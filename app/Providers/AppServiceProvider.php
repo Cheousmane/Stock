842051ed2a9c7b9b37b6e20421bea101b6a477bf
Cheousmane <9k5osama@gmail.com>
2026-07-28 19:29:44 +0000

    fix: forcer HTTPS et configurer TrustProxies pour le déploiement

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -93,6 +93,7 @@ class AppServiceProvider extends ServiceProvider
 
     public function boot(): void
     {
+        \Illuminate\Support\Facades\URL::forceScheme('https');
         Product::observe(ProductObserver::class);
         Invoice::observe(InvoiceObserver::class);
         \App\Models\WarehouseStock::observe(WarehouseStockObserver::class);
